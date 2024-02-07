45f9143052cebbff5c6c88207e667446488f5a56
Tiku Saikia <149768746+tiku-saikia@users.noreply.github.com>
2024-02-07 15:29:26 +0530

    Update repo.js

--- commands/repo.js
+++ commands/repo.js
@@ -1,3 +1,3 @@
 module.exports = async function repoCommand(client, message) {
-    await message.reply("⛩️ *REPO-LINK*\n\nhttps://github.com/tikuchan/Koyuki-\n\n*🍥 Support-Group :* https://chat.whatsapp.com/GgO0xCOqn645x4kedLMCJL\n\n☘️ This is an open source bot anyone can use it ☘️\n\nBot from Crystal🔮");
+    await message.reply("⛩️ *REPO-LINK*\n\nhttps://github.com/tiku-saikia/Koyuki-\n\n*🍥 Support-Group :* https://chat.whatsapp.com/GgO0xCOqn645x4kedLMCJL\n\n☘️ This is an open source bot anyone can use it ☘️\n\nBot from Crystal🔮");
 }
